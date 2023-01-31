d070d19316dd2d7f15ba4d2400ef5e5833814c68
Rahoul Baruah <rahoulb@echodek.co>
2023-01-31 21:23:53 +0000

    Make sure all related models are linked

--- spec/dummy/spec/models/command_spec.rb
+++ spec/dummy/spec/models/command_spec.rb
@@ -3,6 +3,9 @@ require_relative "../rails_helper"
 RSpec.describe StandardProcedure::HasCommands do
   let(:person) { Person.create name: "Trevor Testington" }
   let(:folder) { Folder.create name: "My Documents" }
+  let(:other_folder) { Folder.create name: "Other Documents" }
+  let(:document_1) { Document.create folder: folder, name: "Document 1" }
+  let(:document_2) { Document.create folder: folder, name: "Document 2" }
 
   it "records the actions performed to the command log" do
     Folder.class_eval do
@@ -27,7 +30,21 @@ RSpec.describe StandardProcedure::HasCommands do
     expect(document.actions).to include(action)
   end
 
-  it "links all related models to the command"
+  it "links all related models to the command" do
+    Folder.class_eval do
+      command(:move_document) { |user, params| params[:document].update folder: params[:destination] }
+      authorise(:move_document) { |user| true }
+    end
+
+    person.tells folder, to: :move_document, document: document_1, destination: other_folder
+
+    action = folder.actions.first
+    expect(action).to_not be_nil
+    expect(folder.actions).to include(action)
+    expect(other_folder.actions).to include(action)
+    expect(person.actions).to include(action)
+    expect(document_1.actions).to include(action)
+  end
 
   it "nests commands within the command log"
 
@@ -39,7 +56,7 @@ RSpec.describe StandardProcedure::HasCommands do
     expect { person.tells folder, to: :build_document, name: "testfile.txt" }.to raise_exception(StandardProcedure::Action::Unauthorised)
   end
 
-  it "offers a predefined action for adding to an association" do
+  it "defines a predefined command for adding to an association" do
     Folder.class_eval do
       command :add_document
       authorise(:add_document) { |user| true }
@@ -52,7 +69,7 @@ RSpec.describe StandardProcedure::HasCommands do
     expect(document.name).to eq "testfile.txt"
   end
 
-  it "does not build predefined actions if there is no association with that name" do
+  it "does not build predefined `add` commands if there is no association with that name" do
     Folder.class_eval do
       command(:add_greeting) { |user| "Hello" }
       authorise(:add_greeting) { |user| true }
@@ -60,6 +77,4 @@ RSpec.describe StandardProcedure::HasCommands do
     result = person.tells folder, to: :add_greeting
     expect(result).to eq "Hello"
   end
-  it "offers a predefined action for updating a model"
-  it "offers a predefined action for deleting a model"
 end

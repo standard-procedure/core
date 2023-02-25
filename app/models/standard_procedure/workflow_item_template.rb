91b8c651df9bc565bf08095ae0c66c6925b84b5f
Rahoul Baruah <rahoulb@echodek.co>
2023-02-25 18:18:08 +0000

    Ensure items have field values

--- app/models/standard_procedure/workflow_item_template.rb
+++ app/models/standard_procedure/workflow_item_template.rb
@@ -8,10 +8,15 @@ module StandardProcedure
     belongs_to :account, class_name: "StandardProcedure::Account"
     has_many :items, -> { order :position }, class_name: "StandardProcedure::WorkflowItem", foreign_key: "template_id", dependent: :destroy
 
+    def build_item
+      items.build.with_fields_from(self.fields)
+    end
+
     command :add_item do |user, **params|
       params[:status] ||= params.delete(:workflow).statuses.first
       params[:group] ||= params[:contact].group
-      items.create!(params).tap do |item|
+      build_item.tap do |item|
+        item.update! params 
         item.status.item_added(user, item: item)
       end
     end

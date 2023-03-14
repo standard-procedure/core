57af9f76d01a8f08d035cbdc7bf746e61c6b785b
rahoulb <rahoulb@echodek.co>
2023-03-14 20:43:09 +0000

    Removed ancestry configuration

--- lib/standard_procedure/engine.rb
+++ lib/standard_procedure/engine.rb
@@ -3,6 +3,5 @@ module StandardProcedure
   class Engine < ::Rails::Engine
     isolate_namespace StandardProcedure
     config.generators { |g| g.test_framework :rspec }
-    Ancestry.default_ancestry_format = :materialized_path2
   end
 end

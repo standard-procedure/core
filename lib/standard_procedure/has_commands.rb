2e91f7dd0a0ce3afe50702c03cfe86c2351caf61
rahoulb <rahoulb@echodek.co>
2023-04-27 06:58:24 +0100

    Ensure commands from superclass are available

--- lib/standard_procedure/has_commands.rb
+++ lib/standard_procedure/has_commands.rb
@@ -18,6 +18,56 @@ module StandardProcedure
         end
       end
 
+      def available_commands
+        @available_commands ||= superclass.methods.include?(:available_commands) ? superclass.send(:available_commands) : []
+      end
+
+      def available_commands_for user
+        available_commands.select { |command| authorised_to?(command, user) }.uniq
+      end
+
+      def authorised_to? do_command, user
+        user.respond_to?(:can?) ? user.can?(do_command, self) : false
+      end
+
+      def is_user
+        has_many :performed_commands, class_name: "StandardProcedure::Command", as: :user, dependent: :destroy
+        has_many :notifications, class_name: "StandardProcedure::Notification", as: :user, dependent: :destroy
+
+        define_method :call_stack do
+          @call_stack ||= Concurrent::Array.new
+        end
+
+        define_method :current_context do
+          call_stack.last
+        end
+
+        define_method :build_command_for do |target, command_name: nil, **params|
+          performed_commands.create! target: target, context: current_context, command: "#{target.model_name.singular}_#{command_name}", status: "ready", params: params
+        end
+
+        define_method :acts_on do |target, command: nil, command_name: nil, **params, &implementation|
+          raise ArgumentError "command not supplied" if command.blank?
+          command.update status: "in_progress"
+          call_stack << command
+          begin
+            user = self
+            result = target.instance_eval do
+              target.send :"#{command_name}_implementation", **params.merge(performed_by: user)
+            end
+            command.update! status: "completed", params: params.merge(result: result)
+            result
+          rescue => ex
+            command.update! status: "failed", params: params.merge(error: ex.message)
+            raise ex
+          ensure
+            call_stack.pop
+          end
+        end
+      end
+
+      private
+
       def define_standard_command(command_name, &implementation)
         command_name = command_name.to_sym
         available_commands << command_name unless available_commands.include? command_name
@@ -60,10 +110,6 @@ module StandardProcedure
         end
       end
 
-      def available_commands
-        @available_commands ||= []
-      end
-
       def command_type_for(command_name)
         return :add if is_association_command?(command_name, "add_")
         return :remove if is_association_command?(command_name, "remove_")
@@ -78,50 +124,6 @@ module StandardProcedure
         command_name = command_name.to_s.sub(prefix, "")
         singular ? command_name.to_sym : command_name.pluralize.to_sym
       end
-
-      def is_user
-        has_many :performed_commands, class_name: "StandardProcedure::Command", as: :user, dependent: :destroy
-        has_many :notifications, class_name: "StandardProcedure::Notification", as: :user, dependent: :destroy
-
-        define_method :call_stack do
-          @call_stack ||= Concurrent::Array.new
-        end
-
-        define_method :current_context do
-          call_stack.last
-        end
-
-        define_method :build_command_for do |target, command_name: nil, **params|
-          performed_commands.create! target: target, context: current_context, command: "#{target.model_name.singular}_#{command_name}", status: "ready", params: params
-        end
-
-        define_method :acts_on do |target, command: nil, command_name: nil, **params, &implementation|
-          raise ArgumentError "command not supplied" if command.blank?
-          command.update status: "in_progress"
-          call_stack << command
-          begin
-            user = self
-            result = target.instance_eval do
-              target.send :"#{command_name}_implementation", **params.merge(performed_by: user)
-            end
-            command.update! status: "completed", params: params.merge(result: result)
-            result
-          rescue => ex
-            command.update! status: "failed", params: params.merge(error: ex.message)
-            raise ex
-          ensure
-            call_stack.pop
-          end
-        end
-      end
-    end
-
-    def authorised_to? do_command, user
-      user.respond_to?(:can?) ? user.can?(do_command, self) : false
-    end
-
-    def authorise! do_command, user
-      raise StandardProcedure::Command::Unauthorised unless authorised_to? do_command, user
     end
 
     def available_commands
@@ -129,7 +131,15 @@ module StandardProcedure
     end
 
     def available_commands_for user
-      available_commands.select { |command| authorised_to?(command, user) }.uniq
+      self.class.available_commands_for user
+    end
+
+    def authorised_to? do_command, user
+      self.class.authorised_to? do_command, user
+    end
+
+    def authorise! do_command, user
+      raise StandardProcedure::Command::Unauthorised unless authorised_to? do_command, user
     end
   end
 end

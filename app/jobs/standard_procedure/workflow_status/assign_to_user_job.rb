8a8b255b68271edc63b69136b8ffe04f795e6666
Rahoul Baruah <rahoulb@echodek.co>
2023-05-05 17:53:22 +0100

    Send notifications when statuses are assigned

--- app/jobs/standard_procedure/workflow_status/assign_to_user_job.rb
+++ app/jobs/standard_procedure/workflow_status/assign_to_user_job.rb
@@ -4,6 +4,7 @@ module StandardProcedure
       assign_to = workflow_status.default_contact_for(document)
       return if assign_to.blank?
       document._workflow_assign_to assign_to
+      Notification::SendJob.perform_now recipient: assign_to, details: I18n.t(".item_assigned_to_you"), links: [document]
     end
   end
 end
--- app/models/standard_procedure/notification.rb
+++ app/models/standard_procedure/notification.rb
@@ -1,5 +1,6 @@
 module StandardProcedure
   class Notification < ApplicationRecord
+    scope :unacknowledged, -> { where(acknowledged_at: nil) }
     belongs_to :recipient, polymorphic: true
     enum notification_type: {standard: 0, urgent: 100}
     has_linked :items

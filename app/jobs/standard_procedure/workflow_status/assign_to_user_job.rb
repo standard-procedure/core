710c70cc6fb83193b260cc48e9f2a14d0f6237a0
rahoulb <rahoulb@echodek.co>
2023-05-23 06:06:57 +0100

    Allow for arbitrary reassignments

--- app/jobs/standard_procedure/workflow_status/assign_to_user_job.rb
+++ app/jobs/standard_procedure/workflow_status/assign_to_user_job.rb
@@ -1,7 +1,7 @@
 module StandardProcedure
   class WorkflowStatus::AssignToUserJob < ApplicationJob
-    def perform workflow_status, document:, user:
-      assign_to = workflow_status.default_contact_for(document)
+    def perform workflow_status, document:, user:, assign_to: nil
+      assign_to ||= workflow_status.default_contact_for(document)
       return if assign_to.blank?
       document._workflow_assign_to assign_to
       Notification::SendJob.perform_now recipient: assign_to, details: I18n.t("workflow_status.assign_to_user.item_assigned_to_you"), links: [document]
